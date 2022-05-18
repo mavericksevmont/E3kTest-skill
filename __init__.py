27609275dd9e284b3f110028cf3290dbac1f751c
MaverickSevmont (Erick Sevilla) <sevmont@gmail.com>
2022-05-18 17:54:31 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,15 @@
 from mycroft import MycroftSkill, intent_file_handler
 
 
-class Bark(MycroftSkill):
+class Nite(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('bark.intent')
+    @intent_file_handler('nite.intent')
     def handle_bark(self, message):
-        self.speak_dialog('bark')
+        self.speak_dialog('nite')
 
 
 def create_skill():
-    return Bark()
+    return Nite()
 
